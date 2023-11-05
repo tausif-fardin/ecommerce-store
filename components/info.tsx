401d2519ca3bf06f0efc133fb4cbbd5b8463e0cb
Tausif Fardin <tausiffardin4@gmail.com>
2023-11-05 14:25:37 +0600

    add to cart add

--- components/info.tsx
+++ components/info.tsx
@@ -1,20 +1,28 @@
 'use client';
+
 import { ShoppingCart } from 'lucide-react';
 
+import Currency from '@/components/ui/currency';
+import Button from '@/components/ui/button';
 import { Product } from '@/types';
-import Currency from './ui/currency';
-import Button from './ui/button';
+import useCart from '@/hooks/user-cart';
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className='text-3xl font-bold text-gray-900'>{data.name}</h1>
       <div className='mt-3 flex items-end justify-between'>
-        <p className='text-2xl text-grey-900'>
+        <p className='text-2xl text-gray-900'>
           <Currency value={data?.price} />
         </p>
       </div>
@@ -22,20 +30,20 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       <div className='flex flex-col gap-y-6'>
         <div className='flex items-center gap-x-4'>
           <h3 className='font-semibold text-black'>Size:</h3>
-          <div>{data?.size?.name}</div>
+          <div>{data?.size?.value}</div>
         </div>
         <div className='flex items-center gap-x-4'>
           <h3 className='font-semibold text-black'>Color:</h3>
           <div
-            className='h-7 w-7 rounded-full border border-gray-500'
+            className='h-6 w-6 rounded-full border border-gray-600'
             style={{ backgroundColor: data?.color?.value }}
-          ></div>
+          />
         </div>
       </div>
       <div className='mt-10 flex items-center gap-x-3'>
-        <Button className='flex items-center gap-x-2'>
-          Add to cart
-          <ShoppingCart />
+        <Button onClick={onAddToCart} className='flex items-center gap-x-2'>
+          Add To Cart
+          <ShoppingCart size={20} />
         </Button>
       </div>
     </div>
