4451d00d8e10d97c488a24ed5b3fc75ce33de4fa
Tausif Fardin <tausiffardin4@gmail.com>
2023-11-05 12:39:25 +0600

    cart item number update feature add

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,9 +1,11 @@
-'use client';
-import { useState } from 'react';
-import { ShoppingBag } from 'lucide-react';
+"use client";
 
-import Button from './ui/button';
-import { useEffect } from 'react';
+import { ShoppingBag } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+import Button from "@/components/ui/button";
+import useCart from '@/hooks/user-cart';
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -12,18 +14,28 @@ const NavbarActions = () => {
     setIsMounted(true);
   }, []);
 
+  const router = useRouter();
+  const cart = useCart();
+
   if (!isMounted) {
     return null;
   }
 
-  return (
-    <div className='ml-auto flex items-center gap-x-4'>
-      <Button className='flex items-center rounded-full bg-black px-4 py-2'>
-        <ShoppingBag size={20} color='white' />
-        <span className='ml-2 text-sm font-medium text-white'>0</span>
+  
+
+  return ( 
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button onClick={() => router.push('/cart')} className="flex items-center rounded-full bg-black px-4 py-2">
+        <ShoppingBag
+          size={20}
+          color="white"
+        />
+        <span className="ml-2 text-sm font-medium text-white">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
-};
-
+}
+ 
 export default NavbarActions;
